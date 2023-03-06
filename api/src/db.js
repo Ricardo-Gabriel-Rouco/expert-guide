89445a6eea598cb8bb97c0ccf46f1d24e7138c2e
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-06 15:53:32 -0300

    falto agregado de apikey

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, API_KEY
 } = process.env;
 const axios = require('axios')
 
