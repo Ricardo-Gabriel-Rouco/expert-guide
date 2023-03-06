72cf153a89a5648c6cc7ba3ba17e3395c29edc5c
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-06 04:30:41 -0300

    ultimo detalle

--- api/src/routes/genreRouter.js
+++ api/src/routes/genreRouter.js
@@ -1,9 +1,9 @@
 const {Router} = require('express')
 
 const genresRouter = Router()
-const {createGenres, getAllGenres} = require('../handlers/genresHandler')
+const { getAllGenres} = require('../handlers/genresHandler')
 
-genresRouter.get('/',  createGenres)
+// genresRouter.get('/',  createGenres)
 genresRouter.get('/', getAllGenres)
 
 module.exports = genresRouter
