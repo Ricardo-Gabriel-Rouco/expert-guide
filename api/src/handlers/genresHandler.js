c9bea441b2c782a756a0d311ce97e77c9fe151ef
Gabriel Rouco <r.g.rouco1@gmail.com>
2023-03-06 15:37:28 -0300

    ultimo cambio

--- api/src/handlers/genresHandler.js
+++ api/src/handlers/genresHandler.js
@@ -4,6 +4,7 @@ const {listGenres} = require('../controllers/db/getGenres')
 const getAllGenres = async(req, res ) =>{
   try {
     let genres = await listGenres()
+    console.log(genres)
     if(!genres.length){
       throw new Error('Not Found')
     } else {
@@ -14,14 +15,6 @@ const getAllGenres = async(req, res ) =>{
   }
 }
 
-// const createGenres = async(req, res) => {
-//   try {
-//     const populate = await allGenres()
-//     res.status(200).send(populate)
-//   } catch (error) {
-//     res.status(400).json(error)
-//   }
-// }
 
 module.exports = {
   getAllGenres,
--- api/src/routes/genreRouter.js
+++ api/src/routes/genreRouter.js
@@ -3,7 +3,6 @@ const {Router} = require('express')
 const genresRouter = Router()
 const { getAllGenres} = require('../handlers/genresHandler')
 
-// genresRouter.get('/',  createGenres)
 genresRouter.get('/genres', getAllGenres)
 
 module.exports = genresRouter
